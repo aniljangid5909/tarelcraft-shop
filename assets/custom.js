519cedb1ec8914b9042e64c9c94616dff2765593
Anil Jangid <aniljangid5909@gmail.com>
2026-03-25 14:54:27 +0530

    new update

---
+++ assets/custom.js
@@ -0,0 +1,19 @@
+$('.marquee-slider').slick({
+    speed: 5000,
+    autoplay: true,
+    autoplaySpeed: 0,
+    centerMode: true,
+    cssEase: 'linear',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    infinite: true,
+    initialSlide: 1,
+    arrows: false,
+    buttons: false
+});
+
+
+function gallerySlider(selector,settings) {
+    selector.slick(settings);
+}
